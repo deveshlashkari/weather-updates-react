7b0dc8d372077fc8ee00bc371f4c73e45424cce5
deveshlashkari <deveshlashkari@gmail.com>
2021-11-11 13:48:27 +0530

    initial commit

---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const WEATHER_STACK_API_KEY = "c8942c1659833ae6100d310667c81f76";
+export const COUNTRY_DATA_API_URL = "https://restcountries.com/v3.1/name/";
+export const WEATHER_STACK_API_URL = `http://api.weatherstack.com/current?access_key=${WEATHER_STACK_API_KEY}&query=`;
